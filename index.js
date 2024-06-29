2dba5203cbfd8fa8e3b80ddcbdebe71c2deb561a
Luis-Dati <122989080+Luis-Dati@users.noreply.github.com>
2024-06-29 16:24:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+var express = require("express");
+var cors = require("cors");
+var bodyparser = require("body-parser");
+var app = express();
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+var mysql = require("mysql2");
+var pool = mysql.createPool({
+  host: "bs2yku6jwgpoppvqen9o-mysql.services.clever-cloud.com",
+  user: "uo48jts4vghyypfz",
+  password: "kFdkE08AHZjeDAewwgF6",
+  database: "bs2yku6jwgpoppvqen9o",
+  port: 3306,
+  connectionLimit: 20,
+  waitForConnections: true,
+  queueLimit: 0,
+});
+
+app.listen(3000, () =>
+  console.log("Node server running @ http://localhost:3000"),
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World 1.1");
+});
+
+app.get("/respone", (req, res) => {
+  pool.getConnection(function (err, conn) {
+    if (err) {
+      console.log(err);
+    }
+
+    conn.query("select * from Respone", (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.send(result);
+    });
+
+    pool.releaseConnection(conn);
+  });
+});
+
+app.post("/respone", (req, res) => {
+  pool.getConnection((err, conn) => {
+    if (err) {
+      console.log(err);
+    }
+
+    const params = req.body;
+
+    conn.query("INSERT INTO Respone SET ?", params, (err, result) => {
+      if (!err) {
+        res.send("Inserted item!");
+      } else {
+        console.log(err);
+      }
+    });
+    pool.releaseConnection(conn);
+  });
+});
